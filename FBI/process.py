d5ff4910bbbe84c39359a2818cad0750404e0d70
billetd <ddb524@usask.ca>
2026-06-26 13:16:31 -0600

    Restore comments

--- FBI/process.py
+++ FBI/process.py
@@ -35,25 +35,38 @@ def process(all_data, timerange, lompe_dir, cores=1, med_filter=True, scandelta_
     :param range_times: list[datetime] - Custom "scan" intervals
     """
 
+
+    # If no "range_times"  is given, work it out based on input data
+    # May produce silly scans if there is a mix of normal scan and other modes. Be cautious and only use
+    # if you know what data is going in.
     if not range_times:
+        # Get scan times within timerange, based on whichever radar started earlier
+        # Run the old way if using "scanning data", or the new way if using widebeam data
         if all_data[0][1]['scan'] == 0:
             scan_times, range_times, scan_delta = get_scan_times_old(all_data, timerange)
         else:
             range_times, scan_delta = get_scan_times_widebeam(all_data, timerange)
 
+    # Override scan_delta here to integreate more data per scan
     if scandelta_override is not None:
         scan_delta = scandelta_override
 
     # Initialise an apexpy object, for magnetic transforms
     apex = apexpy.Apex(range_times[0], refh=300)
+
+    # Get the Superdarn grid
+    # This is used for plotting purposes later, e.g. shading darker vectors where there is data
     darn_grid_stuff = grid.sdarn_grid(apex)
 
+    # Remove data we don't use, and make an iterable
     print('Shrinking data...')
     all_data_iterable = all_data_make_iterable(all_data, range_times, scan_delta)
 
+    # Retrive a grid encompassing the SuperDARN Canada PolarDARNs
     canada_grid = lompe_grid_canada(apex)
     del apex   # No longer needed
 
+    # Create Emodel object. Pass grid and Hall/Pedersen conductance functions
     model = lompe.Emodel(canada_grid, Hall_Pedersen_conductance=None, ew_regularization_limit=(50, 75))
     del canada_grid  # No longer needed
 
@@ -138,20 +151,23 @@ def lompe_parallel(scan_time, all_data, scan_delta, darn_grid_stuff, med_filter,
 
     apex = apexpy.Apex(scan_time, refh=300)
 
-    # Extract compact numpy arrays from the raw scan records
+    # Get the data in a format that Lompe likes
     sd_data, rids = prepare_lompe_inputs(apex, all_data, scan_time, scan_delta, med_filter)
 
     del all_data # No longer needed
 
     if sd_data is not None:
+        # Run lompe
         try:
             scan_lompe = run_lompe_model(sd_data, model)
         except IndexError:
             scan_lompe = None
 
+        # Collect the model data to save
         if scan_lompe is not None:
             lompe_data = lompe_extract(scan_lompe, apex, scan_time, darn_grid_stuff, rids)
 
+            # Clean up
             del scan_lompe, sd_data, apex, darn_grid_stuff # No longer needed
             print('Scan complete: ' + scan_time.strftime("%Y-%m-%d %H:%M:%S.%f"))
             return lompe_data
@@ -172,6 +188,7 @@ def prepare_lompe_inputs(apex, all_data, scan_time, scan_delta, med_filter):
     :return:
     """
 
+    # Get data position/value arrays for Lompe
     glat, glon, mlat, mlon, le, ln, le_mag, ln_mag, vlos, vlos_err, rid, ve_mag, vn_mag = (
         get_lompe_data_arrs(apex, all_data, scan_time, scan_delta, med_filter=med_filter))
 
@@ -179,6 +196,7 @@ def prepare_lompe_inputs(apex, all_data, scan_time, scan_delta, med_filter):
     los     = np.vstack((le, ln))
     los_mag = np.vstack((le_mag, ln_mag))
 
+    # Make the Lompe data object
     try:
         sd_data = lompe.Data(vlos, coordinates=coords, LOS=los, LOS_mag=los_mag,
                              datatype='convection', error=vlos_err, iweight=1.0)
@@ -199,6 +217,7 @@ def get_lompe_data_arrs(apex, all_data, scan_time, scan_delta, med_filter=False)
     :return:
     """
 
+    # Arrays that will hold the important parameters
     glon     = []
     glat     = []
     mlons    = []
@@ -215,8 +234,10 @@ def get_lompe_data_arrs(apex, all_data, scan_time, scan_delta, med_filter=False)
 
     for file_index in range(len(all_data)):
 
+        # Station ID
         stid = all_data[file_index][0]['stid']
 
+        # Get the indexes for the records which are within half of of scan_time
         record_times = [
             dt.datetime(
                 all_data[file_index][x]['time.yr'], all_data[file_index][x]['time.mo'],
@@ -231,6 +252,7 @@ def get_lompe_data_arrs(apex, all_data, scan_time, scan_delta, med_filter=False)
 
         for record in times_in_scan:
 
+            # Ranges with data in it, minus ground scatter
             try:
                 slist = all_data[file_index][record]['slist']
             except KeyError:
@@ -240,11 +262,13 @@ def get_lompe_data_arrs(apex, all_data, scan_time, scan_delta, med_filter=False)
 
             gflg = all_data[file_index][record]['gflg']
 
+            # Range seperation and frang
             try:
                 rsep = all_data[file_index][record]['rsep']
             except KeyError:
                 rsep = 45
 
+            # Distance to first range gate
             try:
                 frang = all_data[file_index][record]['frang']
             except KeyError:
@@ -252,35 +276,47 @@ def get_lompe_data_arrs(apex, all_data, scan_time, scan_delta, med_filter=False)
 
             beam = all_data[file_index][record]['bmnum']
 
+            # Iterate over the gates
             for j, gate in enumerate(slist):
+                # Only continue if not ground scatter, velocity is below 2000m/s, and range gates above 10
+                # This removes most erroneous data and near-range (E-region) echos
                 if gflg[j] == 0 and abs(all_data[file_index][record]['v'][j]) <= 2000 and gate > 10:
 
+                    # Median filtering
                     if med_filter is True:
                         vel_range = median_filter(all_data[file_index], record, max_beams, gate)
                     else:
                         vel_range = all_data[file_index][record]['v'][j]
 
+                    # The median filter can fail if unreliable scatter is found
+                    # If so, skip this iteration
                     if not vel_range:
                         continue
 
+                    # Get coordinates of this beam/gate
                     lat, lon = gate2geographic_location(
                         stid=pydarn.RadarID(stid), beam=beam, range_gate=gate,
                         height=300, center=True, rsep=rsep, frang=frang
                     )
                     mlat, mlon = apex.geo2apex(lat, lon, 300)
 
+                    # Kvectors aren't in fitACF files, so we need to calculate it ourselves
+                    # azm = fitacf_get_k_vector(stid, lat, lon, all_data[file_index][record]['v'][j])
+                    # Get the unit vectors in east and west directions
+                    # le_current, ln_current = los_azimuth2en(azm)
                     (le_current, ln_current, le_mag_current, ln_mag_current,
                      ve_geo_current, vn_geo_current,  # returned by the function but not stored
                      ve_mag_current, vn_mag_current) = fitacf_get_k_vector_circle(
                         apex, stid, lat, lon, mlat, mlon, vel_range
                     )
 
+                    # Append to returned lists
                     rid.append(all_data[file_index][record]['stid'])
                     glat.append(lat)
                     glon.append(lon)
                     mlats.append(mlat)
                     mlons.append(mlon)
-                    vlos.append(abs(vel_range))
+                    vlos.append(abs(vel_range)) # Needs to be magnitude of the velocity, sign is handled by azimuth
                     vlos_err.append(abs(all_data[file_index][record]['v_e'][j]))
                     le.append(le_current)
                     ln.append(ln_current)
@@ -299,11 +335,15 @@ def run_lompe_model(sd_data, model):
     """
     Segregated code to run the lompe model
     """
+
+    # Add all the vectors to the model object
     model.add_data(sd_data)
 
+    # Run inversion
     try:
         model.run_inversion(l1=10, l2=0.1, lapack_driver='gelsy')
     except TypeError:
+        # I had the run break on inversion randomly once. Not sure why.
         model = None
 
     return model
